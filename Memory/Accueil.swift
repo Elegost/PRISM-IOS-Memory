5a9ae56bd28c4e1f2e2cd96d5284aba79599b20f
E.L.3.G.O.S.T <EL3GOST@MacBook-Pro-de-EL3GOST.local>
2016-05-20 22:38:58 +0200

    Version finale

--- Memory/Accueil.swift
+++ Memory/Accueil.swift
@@ -46,15 +46,20 @@ class Accueil: UIViewController{
             textField_familyName.hidden = true
             button_launch.hidden = true
             button_logIn.hidden = false
+            button_SeConnecter.hidden = true
             button_Connection.setTitle("Pas encore inscrit ? Veuillez créer un compte", forState: UIControlState.Normal)
         }
         else
         {
             label_Username.text = "Bonjour " + usernameLogConn.name + " " + usernameLogConn.familyName + " (" + usernameLogConn.username + ")"
             button_launch.setTitle("Jouer", forState: UIControlState.Normal)
+            textField_name.text = usernameLogConn.name
+            textField_Username.text = usernameLogConn.username
+            textField_familyName.text = usernameLogConn.familyName
             button_launch.hidden = false
             button_Score.hidden = false
             button_logIn.hidden = true
+            button_SeConnecter.hidden = true
             textField_Username.hidden = true;
             textField_name.hidden = true
             textField_familyName.hidden = true
@@ -79,6 +84,21 @@ class Accueil: UIViewController{
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var button_SeConnecter: UIButton!
+    @IBAction func button_seConnecter_click(sender: UIButton)
+    {
+        if(textField_Username.text != "")
+        {
+            s_lc.username = textField_Username.text!
+            s_lc.name = textField_name.text!
+            s_lc.familyName = textField_familyName.text!
+            logConn.WriteToFile(s_lcToString())
+            usernameLogConn = logConn.ReadFromFile()
+            button_launch.hidden = false
+            button_SeConnecter.hidden = true
+            button_Score.hidden = false
+        }
+    }
 
     /*
     // MARK: - Navigation
@@ -101,11 +121,11 @@ class Accueil: UIViewController{
         }
         if segue.identifier == "SegueToMain"
         {
+            logConn.WriteToFile(s_lcToString())
             // Definition du controler de destinatation
             let destinationVC = segue.destinationViewController as? ViewController
             // Transfert de données
             destinationVC?.pseudoUsername = textField_Username.text!
-            logConn.WriteToFile(s_lcToString())
         }
     }
 
@@ -115,8 +135,10 @@ class Accueil: UIViewController{
         textField_name.hidden = false
         textField_familyName.hidden = false
         label_Username.hidden = true
-        button_launch.hidden = false
+        button_launch.hidden = true
         button_logIn.hidden = true
+        button_SeConnecter.hidden = false
+        button_Score.hidden = true
     }
 
     @IBAction func button_Score_click(sender: UIButton)
